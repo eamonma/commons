b26bb15fb27dc652c9301f489cc9b3cd5f12d6d7
Eamon Ma <m@eamonma.com>
2019-04-18 21:02:37 -0700

    UI enhancements

--- public/index.js
+++ public/index.js
@@ -64,7 +64,7 @@ var schedule = {
         },
         setCourses: function (courses, name) {
             name = name || "courses"
-            return localStorage.setItem(name, JSON.stringify(courses));
+            return localStorage.setItem(name, courses);
         },
         getCourses: function (name) {
             name = name || "courses"
